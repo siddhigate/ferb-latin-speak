2cb10abebdff5d55b4b6fdacc78e12f8f2cfe04e
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2021-09-19 09:16:41 +0530

    fix: empty input and reset error output

--- app.js
+++ app.js
@@ -3,34 +3,36 @@ var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 var errorDiv = document.querySelector("#errorMessage");
 
-
 // server url
-var serverUrl= "https://api.funtranslations.com/translate/ferb-latin.json";
+var serverUrl = "https://api.funtranslations.com/translate/ferb-latin.json";
 
 // formatting url
-function getTranslationUrl(text){
-    return serverUrl +"?"+"text="+text;
+function getTranslationUrl(text) {
+    return serverUrl + "?" + "text=" + text;
 }
 
 // error handling
-function errorHandler(error){
-    errorDiv.innerText="We are sorry. You can only use this for 5 times per hour. Try again later.";
+function errorHandler(error) {
+    errorDiv.innerText =
+        "We are sorry. You can only use this for 5 times per hour. Try again later.";
 }
 
 // click event
-btnTranslate.addEventListener("click", clickEventHandler())
+btnTranslate.addEventListener("click", () => {
+    errorDiv.innerText = "";
+    outputDiv.innerText = "";
+    clickEventHandler();
+});
 
-// handiling event displaying output
+// handling event displaying output
 function clickEventHandler() {
-    return function clickEventHandler() {
-        var inputText = txtInput.value;
-
+    var inputText = txtInput.value;
+    if (inputText === "") {
+        errorDiv.innerText = "Please enter some text";
+    } else {
         fetch(getTranslationUrl(inputText))
-        .then(response=>response.json())
-        .then(json => outputDiv.innerText=json.contents.translated)
-        .catch(errorHandler)
-        ;
-
-    };
+            .then((response) => response.json())
+            .then((json) => (outputDiv.innerText = json.contents.translated))
+            .catch(errorHandler);
+    }
 }
-
